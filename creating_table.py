a08c02f26bb9996193a3d6cef6fe8016f518783e
oktavianuu <oktavianuss.misro@gmail.com>
2024-03-13 09:51:04 +0700

    create table in db

--- creating_table.py
+++ creating_table.py
@@ -2,7 +2,9 @@ import sqlite3
 
 conn = sqlite3.connect('todo.db')
 c = conn.cursor()
-c.execute('''CREATE TABLE tasks (
+# execute CREATE TABLE in our database
+# the execure method takes any single SQL statement and optional parameter necessary to execute the query
+c.execute('''CREATE TABLE tasks ( 
           id INTEGER PRIMARY KEY,
           name TEXT NOT NULL,
           priority INTEGER NOT NULL
