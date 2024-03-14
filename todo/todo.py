e62f74e0d23dc4c0da8f45e80c28fb996c4ba6df
oktavianuu <oktavianuss.misro@gmail.com>
2024-03-14 20:57:24 +0700

    update things

--- todo/todo.py
+++ todo/todo.py
@@ -5,27 +5,48 @@ class Todo:
         self.conn = sqlite3.connect('todo.db')
         self.c = self.conn.cursor()
         self.create_task_table()
-        self.find_task()
-        self.show_task()
     
     def create_task_table(self):
-        self.c.execute('''CREATE TABLE tasks (
+        self.c.execute('''CREATE TABLE IF NOT EXISTS tasks (
                        id INTEGER PRIMARY KEY,
-                       name TEXT NOT NULL,
-                       priority INTEGER NOT NULL
+                       name TEXT NOT NULL UNIQUE,
+                       priority INTEGER NOT NULL CHECK(priority >= 1)
         );''')
+        self.conn.commit()
     
     def add_task(self):
-        name = input('Enter task name: ')
-        priority = int(input("Enter priority: "))
-
-        self.c.execute('INSERT INTO tasks (name, priority)')
+        name = input('Enter task name: ').strip() # remove leading/trailing whitespace
+        if not name:
+            print("The task name cannot be an empty string.")
+        return 
     
-    def find_task(self):
-        None
-
-    def show_task(self):
+        try:
+            priority = int(input("Enter priority"))
+            if priority < 1:
+                print("task priority must be at least 1.")
+                return
+        except ValueError:
+            print("Priority must be a number.")
+            return
         
+        if self.find_task(name) is not None:
+            print(" A task with the same name already exists.")
+            return
+        
+        self.c.execute('INSERT INTO tasks (name, priority) VALUES (?, ?)', (name, priority))
+        self.conn.commit()
+        print("Task added successfully.")
     
+    def find_task(self, task_name):
+        self.c.execute('SELECT * FROM tasks WHERE name = ?', (task_name, ))
+        return self.c.fetchone()
+    
+    def show_tasks(self):
+        self.c.execute('SELECT * FROM tasks')
+        tasks = self.c.fetchall()
+        for task in tasks:
+            print(task)
+
 app = Todo()
 app.add_task()
+app.find_task()
